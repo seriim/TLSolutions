7d20beac408391beccee7cdc0fe01b32a695b581
Serena Morris <serenamorris@Serenas-MacBook-Pro.local>
2025-03-05 15:43:09 -0500

    added google analytics

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue';
 import HomeView from '@/views/HomeView.vue'
 import VisaView from '@/views/VisaView.vue'
 import AboutView from '@/views/AboutView.vue'
